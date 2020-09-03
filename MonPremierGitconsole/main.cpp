86b0bbf6a9f25687fd634421cd2e7356d7c9431f
kuro0499 <70703911+kuro0499@users.noreply.github.com>
2020-09-03 15:43:32 +0200

    Revert "T-max en y"
    This reverts commit 882bc4a9f02c310aa39f98d27997e4a81fd66d46.

--- MonPremierGitconsole/main.cpp
+++ MonPremierGitconsole/main.cpp
@@ -1,11 +1,10 @@
 #include <QCoreApplication>
 #include <iostream>
 using namespace std;
-
 int main(int argc, char *argv[])
 {
-    cout<<"En y"<<endl;
-    cout<<"T-Max en Y"<<endl;
+    cout<<"WEEE PUUUUU"<<endl;
     QCoreApplication a(argc, argv);
+
     return a.exec();
 }
